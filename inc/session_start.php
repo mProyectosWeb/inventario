e26695c827a096a59b564a9cdec346cda2134e4b
mProyectosWeb <109919965+mProyectosWeb@users.noreply.github.com>
2023-07-04 18:47:08 -0600

    Inventario fase1 v1

---
+++ inc/session_start.php
@@ -0,0 +1,4 @@
+<?php 
+session_name("IV");
+session_start();
+?>
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php include_once "./inc/navbar.php"; ?>
+<?php require_once "./inc/session_start.php"?>
+
+<?php 
+
+    if(!isset($_GET['views']) || $_GET['views']==""){
+        $_GET['views']="login";
+    }
     
-<?php include_once "./inc/footer.php"; ?>
+    if(is_file("./views/".$_GET['views'].".php") && $_GET['views'] != "login" && $_GET['views'] != "login"){
+        include_once "./inc/navbar.php";
+        include_once "./views/".$_GET['views'].".php";
+        include_once "./inc/footer.php";     
+    }else{
+        if($_GET['views']=="login"){
+            include_once "./views/login.php";
+        }else{
+            include_once "./views/404.php";
+        }
+    }
+?>
