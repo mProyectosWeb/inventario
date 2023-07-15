a746b4c1f729efb02aa3565d79da025241710dd6
mProyectosWeb <m.angel.desarrolladorweb@gmail.com>
2023-07-15 13:34:22 -0600

    Usuario terminado

--- php/user_new.php
+++ php/user_new.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once "../php/main.php";
-    
+
     #Almacenando datos
     $nombre = limpiar_cadena($_POST['usuario_nombre']);
     $apellido = limpiar_cadena($_POST['usuario_apellido']);
@@ -11,86 +11,147 @@
     $clave_2 = limpiar_cadena($_POST['usuario_clave_2']);
 
     #Verificando campos obligatorios
-    if($nombre=="" || $apellido=="" || $usuario=="" || $email=="" || $clave_1=="" || $clave_2==""){
+    if ($nombre == "" || $apellido == "" || $usuario == "" || $email == "" || $clave_1 == "" || $clave_2 == "") {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            No has llenado todos los campos que son obligatorios
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                No has llenado todos los campos que son obligatorios
+            </div>
+            ';
         exit();
     }
 
-    if(verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$nombre)){
+    if (verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}", $nombre)) {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            El NOMBRE no coincide con el formato solicitado
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El NOMBRE no coincide con el formato solicitado
+            </div>
+            ';
         exit();
     }
 
-    if(verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$apellido)){
+    if (verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}", $apellido)) {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            El APELLIDO no coincide con el formato solicitado
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El APELLIDO no coincide con el formato solicitado
+            </div>
+            ';
         exit();
     }
-    
-    if(verificar_datos("[a-zA-Z0-9]{4,20}",$usuario)){
+
+    if (verificar_datos("[a-zA-Z0-9]{4,20}", $usuario)) {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            El USUARIO no coincide con el formato solicitado
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El USUARIO no coincide con el formato solicitado
+            </div>
+            ';
         exit();
     }
-    
-    if(verificar_datos("[a-zA-Z0-9]{4,20}",$usuario)){
+
+    if (verificar_datos("[a-zA-Z0-9]{4,20}", $usuario)) {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            El USUARIO no coincide con el formato solicitado
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El USUARIO no coincide con el formato solicitado
+            </div>
+            ';
         exit();
     }
-    
-    if(verificar_datos("[a-zA-Z0-9$@.-]{7,100}",$clave_1) || verificar_datos("[a-zA-Z0-9$@.-]{7,100}",$clave_2)){
+
+    if (verificar_datos("[a-zA-Z0-9$@.-]{7,100}", $clave_1) || verificar_datos("[a-zA-Z0-9$@.-]{7,100}", $clave_2)) {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            La CLAVE no coincide con el formato solicitado
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                La CLAVE no coincide con el formato solicitado
+            </div>
+            ';
         exit();
     }
 
     #Verificacndo el email
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $check_email=conexion();
-        $check_email=$check_email->query("SELECT usuario_email FROM usuario WHERE usuario_email='$email'");
-        if($check_email->rowCount()>0){
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $check_email = conexion();
+        $check_email = $check_email->query("SELECT usuario_email FROM usuario WHERE usuario_email='$email'");
+        if ($check_email->rowCount() > 0) {
             echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            El CORREO ya se encuentra registrado, por favor elija otro
-        </div>
-        ';
-        exit();
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El CORREO ya se encuentra registrado, por favor elija otro
+            </div>
+            ';
+            exit();
         }
-        $check_email=null;
-    }else{
+        $check_email = null;
+    } else {
         echo '
-        <div class="notification is-danger is-light">
-            <strong>¡Ocurrio un error inesperado!</strong><br>
-            El CORREO ingresado no es valido
-        </div>
-        ';
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El CORREO ingresado no es valido
+            </div>
+            ';
         exit();
     }
+
+    #Verificando usuario
+    $check_usuario = conexion();
+    $check_usuario = $check_usuario->query("SELECT usuario_usuario FROM usuario WHERE usuario_usuario='$usuario'");
+    if ($check_usuario->rowCount() > 0) {
+        echo '
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                El USUARIO ya se encuentra registrado, por favor elija otro
+            </div>
+            ';
+        exit();
+    }
+    $check_usuario = null;
+
+    #Verificando las claves
+    if($clave_1 != $clave_2){
+        echo '
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                Las CLAVES que ha ingresado no coinciden
+            </div>
+            ';
+        exit();
+    }else{
+        $clave=password_hash($clave_1, PASSWORD_BCRYPT, ["cost"=>10]);
+    }
+
+    #Guardando los datos
+    $guardar_usuario = conexion();
+    $guardar_usuario = $guardar_usuario->prepare("INSERT INTO usuario(usuario_nombre,usuario_apellido,usuario_usuario,usuario_clave,usuario_email) VALUES(:nombre,:apellido,:usuario,:clave,:email)");
+    
+    $marcadores=[
+        ":nombre"=>$nombre,
+        ":apellido"=>$apellido,
+        ":usuario"=>$usuario,
+        ":clave"=>$clave,
+        ":email"=>$email
+    ];
+    
+    $guardar_usuario->execute($marcadores);
+    
+    if($guardar_usuario->rowCount() == 1){
+        echo '
+            <div class="notification is-info is-light">
+                <strong>¡Usuario registrado!</strong><br>
+                El usuario se registro con exito
+            </div>
+            ';
+        exit();
+    }else{
+        echo '
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                No se pudo registrar el usuario, por favor intente nuevamente
+            </div>
+            ';
+        exit();
+    }
+
+    $guardar_usuario=null;
